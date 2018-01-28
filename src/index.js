759a5a154fa392f298a0dfcae53ee641525bdc05
GousosGas <msf7415003@teilar.gr>
2018-01-28 12:18:10 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Component/Main';
+import './styles/stylesheet.css';
+
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
+
