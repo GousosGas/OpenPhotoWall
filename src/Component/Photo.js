4e1b8a8dea1d11b4b94cffb3c20af2295dd6d4f9
GousosGas <msf7415003@teilar.gr>
2018-01-28 13:22:11 +0200

    btn  commit

--- src/Component/Photo.js
+++ src/Component/Photo.js
@@ -5,6 +5,8 @@ class Photo extends Component{
         const post =this.props.post;
         return <figure className="figure">
             <img class="photo" src={post.imageLink} alt={post.description}/>
+            <figcaption><p>{post.description}</p> </figcaption>
+            <button>Remove</button>
         </figure>
     }
 }
